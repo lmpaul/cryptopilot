57b5d8d7d3e524fb18552fca9d0e82064544e6b6
Nathan Fitger <nath.fitger@gmail.com>
2021-11-23 15:53:53 +0100

    assets seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "uri"
+require 'json'
+require 'open-uri'
+
+limit = 500
+res_api = URI.open("https://pro-api.coinmarketcap.com/v1/cryptocurrency/map",
+                  'X-CMC_PRO_API_KEY' => '0afa05e5-36e7-4bdd-b0f7-318edac9d63d')
+json = JSON.parse(res_api.read)
+json["data"].each do |d|
+  Asset.create(coin_mcap_id: d["id"], rank: d["rank"], name: d["name"], symbol: d["symbol"]) if d["rank"] < limit
+end
+p "created #{limit} assets"
