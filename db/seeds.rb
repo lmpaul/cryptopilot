e670d4412c65d831e9d251bec09fc89777b1dc71
Nathan Fitger <nath.fitger@gmail.com>
2021-11-23 16:22:40 +0100

    gem added in gem file (not usigin require)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require "uri"
-require 'json'
-require 'open-uri'
 
 limit = 500
 res_api = URI.open("https://pro-api.coinmarketcap.com/v1/cryptocurrency/map",
