a20dc35c2cfe26eff7d873e3f52ce6fcbbe21e7a
Paul Le Mercier <paul.lemercier05@gmail.com>
2021-11-23 16:31:38 +0100

    super model

--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -1,28 +1,29 @@
 class Dashboard < ApplicationRecord
+  serialize :asset
   belongs_to :user
   has_many :transactions
 
   def check_transactions_equality
     registered_transactions = self.transactions
-    counted_transactions = Transaction.where("dashboard_id = #{self.id}").length
+    counted_transactions = @transactions.length
     registered_transactions == counted_transactions
   end
 
   def assets_id_list
     @assets_id_list = []
     @transactions.each do |transaction|
-      if @assets_id_list.include?(Asset.find(transaction.asset_id))
+      if @assets_id_list.include?(Asset.find(transaction.asset_id).id)
         next
       else
-        @assets_id_list << Asset.find(transaction.asset_id)
+        @assets_id_list << Asset.find(transaction.asset_id).id
       end
     end
   end
 
   def asset_qty(asset_id)
-    asset_transactions = Transaction.where("asset_id = #{asset_id}")
-    buy_transactions = asset_transactions.where("direction = buy")
-    sell_transactions = asset_transactions.where("direction = sell")
+    asset_transactions = @transactions.where("asset_id = #{asset_id}")
+    buy_transactions = asset_transactions.where("direction = 'buy'")
+    sell_transactions = asset_transactions.where("direction = 'sell'")
     buying_sum = 0
     buy_transactions.each do |transaction|
       buying_sum += transaction.quantity
@@ -35,8 +36,8 @@ class Dashboard < ApplicationRecord
   end
 
   def asset_total_spent(asset_id)
-    asset_transactions = Transaction.where("asset_id = #{asset_id}")
-    buy_transactions = asset_transactions.where("direction = buy")
+    asset_transactions = @transactions.where("asset_id = #{asset_id}")
+    buy_transactions = asset_transactions.where("direction = 'buy'")
     total_spent = 0
     buy_transactions.each do |transaction|
       total_spent += (transaction.quantity * transaction.price)
@@ -44,44 +45,54 @@ class Dashboard < ApplicationRecord
     return total_spent
   end
 
+  def tokens_bought(asset_id)
+    asset_transactions = @transactions.where("asset_id = #{asset_id}")
+    buy_transactions = asset_transactions.where("direction = 'buy'")
+    token_bought = 0
+    buy_transactions.each do |transaction|
+      token_bought += transaction.quantity
+    end
+    return token_bought
+  end
+
   def create_hash
     @transactions = Transaction.where("dashboard_id = #{self.id}")
     assets_id_list
     assets = {}
     @assets_id_list.each do |asset_id|
-      assets[asset.to_sym] = {
+      assets[asset_id.to_s] = {
         name: Asset.find(asset_id).name,
         # Compute quantity for each
         quantity: asset_qty(asset_id),
         # Compute total spent for each
-        total_spent: total_spent(asset_id),
+        total_spent: asset_total_spent(asset_id),
         # Compute number of transactions
-        number_of_transaction: Transactions.where("asset_id = #{asset_id}").length,
+        number_of_transaction: Transaction.where("asset_id = #{asset_id}").length,
         # Compute average cost
-        average_cost: (total_spent(asset_id) / asset_qty(asset_id))
+        average_cost: (asset_total_spent(asset_id) / tokens_bought(asset_id))
       }
     end
+    return assets
   end
 
   def assets_market_price
     # ! Use real price when API calls are ready
-    @assets_keys.each do |asset|
-      @assets[asset.to_sym][market_price] = 1000
+    @assets_keys.each do |asset_key|
+      @assets[asset_key.to_s][:market_price] = 1000
     end
   end
 
   def assets_pnl
     @assets_keys.each do |asset_key|
-      asset = @assets[asset_key.to_sym]
-      asset[pnl] = (asset[market_price] - asset[average_cost]) * asset[quantity]
+      asset = @assets[asset_key.to_s]
+      asset[:pnl] = (asset[:market_price] - asset[:average_cost]) * asset[:quantity]
     end
   end
 
   def define_assets
     if self.asset.nil?
       # Create the hash
-      self.asset = create_hash
-      @assets = self.asset
+      @assets = create_hash
       @assets_keys = @assets.keys
     else
       if check_transactions_equality
@@ -96,5 +107,6 @@ class Dashboard < ApplicationRecord
     end
     assets_market_price
     assets_pnl
+    p @assets
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create a user
+# Create a dashboard
+# Create 1 assets
+# Create 4 transaction
+
+u = User.new(email: "paul@cryptopilot.com", username: "Paul", password: "azerty")
+u.save
+
+d = Dashboard.new(name: "Dashboard de test", user_id: 1)
+d.save
+
+a = Asset.new(coin_mcap_id: 1, rank: 1, name: "bitcoin", symbol:"BTC")
+a.save
+
+a = Asset.new(coin_mcap_id: 2, rank: 20, name: "litecoin", symbol:"LTC")
+a.save
+
+t = Transaction.new(direction: "buy", asset_name: "litecoin", quantity: "1", price: "100", date: "2021-11-20", dashboard_id: 1, asset_id: 2)
+t.save
+t = Transaction.new(direction: "buy", asset_name: "bitcoin", quantity: "4", price: "500", date: "2021-11-10", dashboard_id: 1, asset_id: 1)
+t.save
+t = Transaction.new(direction: "sell", asset_name: "bitcoin", quantity: "1", price: "2500", date: "2021-11-22", dashboard_id: 1, asset_id: 1)
+t.save
+t = Transaction.new(direction: "sell", asset_name: "bitcoin", quantity: "1", price: "3000", date: "2021-11-23", dashboard_id: 1, asset_id: 1)
+t.save
+puts "✅ Databse seeded!"
