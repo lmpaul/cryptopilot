0bbd5ca9b33ecd3150d540786f2acd7f4461691f
Nathan Fitger <nath.fitger@gmail.com>
2021-11-23 17:47:29 +0100

    commit

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,2 +1,29 @@
 class TransactionsController < ApplicationController
+  def new
+    @dashboard = Dashboard.find(params[:dashboard_id])
+    @transaction = Transaction.new
+  end
+
+  def create
+    @dashboard = Dashboard.find(transaction_params[:dashboard])
+    @transaction = Transaction.new(direction: transaction_params[:direction],
+                         dashboard_id: @dashboard.id,
+                         user_id: current_user.id,
+                         asset_id: transaction_params[:asset_id],
+                         price: transaction_params[:price],
+                         quantity: transaction_params[:quantity],
+                         asset_name: Asset.find(transaction_params[:asset_id]),
+                         date: Date.today)
+    if @transaction.save
+      redirect_to dashboard_path(@dashboard)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def transaction_params
+    params.require(:transaction).permit(:direction, :asset_id, :dashboard, :price)
+  end
 end
