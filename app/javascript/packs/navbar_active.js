b2faa662e2c401344fbae5874b3d54b88ceb988f
Jeremy Melieres <melieresjeremy@gmail.com>
2021-11-29 14:16:14 +0100

    add active navbar + front

---
+++ app/javascript/packs/navbar_active.js
@@ -0,0 +1,16 @@
+const navbarActive = () => {
+  const path = window.location.pathname;
+  const navLinks = document.querySelectorAll('.navbar-buttons');
+  if (navLinks) {
+    navLinks.forEach(link => link.classList.remove('nav-active'))
+    if (path === '/') {
+      navLinks[0].classList.add('nav-active')
+    } else if (path.split('/').includes('dashboards')) {
+      navLinks[1].classList.add('nav-active')
+    } else if (path.includes('notes')) {
+      navLinks[2].classList.add('nav-active')
+    }
+  }
+}
+
+export { navbarActive }
