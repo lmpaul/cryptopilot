e75162112a89fbdb7e5eff9094c7d49f7b346dd2
Jeremy Melieres <melieresjeremy@gmail.com>
2021-11-30 12:55:47 +0100

    add ressources tab

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
   resources :notes
+  resources :ressources
   resources :dashboards do
     resources :transactions
     get '/charts/values', to: 'charts#values'
