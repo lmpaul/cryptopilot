7d7eb353b458072cf4fbc8d75c7ebd82b6fb48e6
Paul Le Mercier <paul.lemercier05@gmail.com>
2021-11-30 15:53:19 +0100

    optimized model and controllers

--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -4,47 +4,12 @@ class Dashboard < ApplicationRecord
   has_many :transactions, dependent: :destroy
   validates :name, presence: true
 
-  # Permet de récupérer la liste des id des assets actuellement dans le pilot
-  # ! Il faut gérer le cas où un actif revient à zero
-  def assets_id_list
-    @assets_id_list = []
-    transactions = Transaction.where("dashboard_id = #{self.id}")
-    transactions.each do |transaction|
-      if @assets_id_list.include?(Asset.find(transaction.asset_id).id)
-        next
-      else
-        @assets_id_list << Asset.find(transaction.asset_id).id
-      end
-    end
+  def average_cost_price(asset_id, date)
+    asset_total_spent(asset_id) / tokens_bought(asset_id, date)
   end
 
-  # Permet de calculer le nombre de coins actuellement dans le pilot pour chaque asset
-  def asset_qty(asset_id, date)
-    transactions = Transaction.where("dashboard_id = #{self.id}")
-    asset_transactions = transactions.where("asset_id = #{asset_id} AND date <= ?", date)
-    buy_transactions = asset_transactions.where("direction = 'Buy'")
-    sell_transactions = asset_transactions.where("direction = 'Sell'")
-    buying_sum = 0
-    buy_transactions.each do |transaction|
-      buying_sum += transaction.quantity
-    end
-    selling_sum = 0
-    sell_transactions.each do |transaction|
-      selling_sum += transaction.quantity
-    end
-    return buying_sum - selling_sum
-  end
-
-  # Permet de calculer le montant dépensé dans un asset depuis le début du pilot
-  def asset_total_spent(asset_id)
-    transactions = Transaction.where("dashboard_id = #{self.id}")
-    asset_transactions = transactions.where("asset_id = #{asset_id}")
-    buy_transactions = asset_transactions.where("direction = 'Buy'")
-    total_spent = 0
-    buy_transactions.each do |transaction|
-      total_spent += (transaction.quantity * transaction.price)
-    end
-    return total_spent
+  def market_price(asset_id, date)
+    return PriceHistory.where("id_name = '#{Asset.find(asset_id).id_name}' AND date = ?", date).last.price
   end
 
   # Permet de cacluler le nombre de token achetés pour un actif depuis le début
@@ -59,87 +24,133 @@ class Dashboard < ApplicationRecord
     return token_bought
   end
 
+  # Permet de calculer le montant dépensé dans un asset depuis le début du pilot
+  def asset_total_spent(asset_id)
+    asset_transactions = @transactions.where("asset_id = #{asset_id}")
+    buy_transactions = asset_transactions.where("direction = 'Buy'")
+    total_spent = 0
+    buy_transactions.each do |transaction|
+      total_spent += (transaction.quantity * transaction.price)
+    end
+    return total_spent
+  end
+
+  def compute_buying_sum(buy_transactions)
+    buying_sum = 0
+    buy_transactions.each do |transaction|
+      buying_sum += transaction.quantity
+    end
+    return buying_sum
+  end
+
+  def compute_selling_sum(sell_transactions)
+    selling_sum = 0
+    sell_transactions.each do |transaction|
+      selling_sum += transaction.quantity
+    end
+    return selling_sum
+  end
+
+  # Permet de calculer le nombre de coins actuellement dans le pilot pour chaque asset
+  def asset_qty(asset_id, date)
+    asset_transactions = @transactions.where("asset_id = #{asset_id} AND date <= ?", date)
+    buy_transactions = asset_transactions.where("direction = 'Buy'")
+    sell_transactions = asset_transactions.where("direction = 'Sell'")
+    buying_sum = compute_buying_sum(buy_transactions)
+    selling_sum = compute_selling_sum(sell_transactions)
+    return buying_sum - selling_sum
+  end
+
   # Permet de créer la première partie du hash pour ce qu'il y a aujourd'hui dans le pilot
   def create_hash(date)
-    assets_id_list
     assets = {}
     @assets_id_list.each do |asset_id|
       assets[Asset.find(asset_id).id_name] = {
         id: asset_id,
         quantity: asset_qty(asset_id, date),
         total_spent: asset_total_spent(asset_id),
-        number_of_transaction: Transaction.where("dashboard_id = #{self.id} AND asset_id = #{asset_id}").length,
-        average_cost: tokens_bought(asset_id,date) == 0 ? 0 : (asset_total_spent(asset_id) / tokens_bought(asset_id, date)),
-        market_price: PriceHistory.where("id_name = '#{Asset.find(asset_id).id_name}' AND date = ?", date).last.price,
-        pnl: (PriceHistory.where("id_name = '#{Asset.find(asset_id).id_name}' AND date = ?", date).last.price) * asset_qty(asset_id, date) - (asset_total_spent(asset_id) / tokens_bought(asset_id, date) * asset_qty(asset_id, date)),
+        number_of_transaction: @transactions.where("asset_id = #{asset_id}").length,
+        average_cost:
+        if tokens_bought(asset_id, date).zero?
+          0
+        else
+          average_cost_price(asset_id, date)
+        end,
+        market_price: market_price(asset_id, date),
+        pnl:
+        (market_price(asset_id, date) * asset_qty(asset_id, date)) -
+        (average_cost_price(asset_id, date) * asset_qty(asset_id, date)),
         date: date
       }
     end
     return assets
   end
 
+  # Permet de récupérer la liste des id des assets actuellement dans le pilot
+  def assets_id_list
+    @assets_id_list = []
+    transactions = Transaction.where("dashboard_id = #{self.id}")
+    transactions.each do |transaction|
+      next if @assets_id_list.include?(Asset.find(transaction.asset_id).id)
+
+      @assets_id_list << Asset.find(transaction.asset_id).id
+    end
+  end
+
+  # Permet de metter à jour la table des historiques des prix par asset et par date
+  def update_price_history(asset, json)
+    json["prices"].each do |element|
+      price = PriceHistory.find_by(date: Time.at(element[0]/1000).to_date, id_name: asset.id_name)
+      if price.nil?
+        PriceHistory.create(id_name: asset.id_name, date: Time.at(element[0]/1000).to_date, price: element[1], asset_id: asset.id)
+      else
+        price.update(id_name: asset.id_name, date: Time.at(element[0]/1000).to_date, price: element[1], asset_id: asset.id)
+      end
+    end
+  end
+
   # Permet d'obtenir l'historique des prix des actifs du wallet
   def get_history(number_of_days)
-    #chercher le price de chaques asset par dates et le mettre dans Pricehistorys
     vs_currency = "USD"
-    assets_id_list
-    # PriceHistory.group(:date).having("count(*) > 1").destroy_all
+    assets_id_list # permet de définir @assets_id_list
     @assets_id_list.each do |asset_key|
       asset = Asset.find(asset_key.to_i)
       res_api = URI.open("https://api.coingecko.com/api/v3/coins/#{asset.id_name}/market_chart?vs_currency=#{vs_currency}&days=#{number_of_days}&interval=daily")
       json = JSON.parse(res_api.read)
       json["prices"].delete_at(-2)
-      json["prices"].each do |element|
-        price = PriceHistory.find_by(date: Time.at(element[0]/1000).to_date, id_name: asset.id_name)
-        if price.nil?
-          PriceHistory.create(id_name: asset.id_name, date: Time.at(element[0]/1000).to_date, price: element[1], asset_id: asset.id)
-        else
-          price.update(id_name: asset.id_name, date: Time.at(element[0]/1000).to_date, price: element[1], asset_id: asset.id)
-        end
-      end
+      update_price_history(asset, json)
     end
   end
 
-
-  # Permet de calculer le nombre de jour depuis la première tx à aujourd'hui
-  def compute_number_of_days
-    transactions = self.transactions.order('date ASC')
+  # Permet de récupérer toutes les dates depuis la première transac à aujourd'hui + le nb de jour que cela représente
+  def define_dates(transactions)
     first_date = transactions.first.date
     number_of_days = (Date.today - first_date).to_i + 1
-
-    return number_of_days
-  end
-
-  # Permet de récupérer toutes les dates depuis la première transac à aujourd'hui
-  def get_dates
-    transactions = self.transactions.order('date ASC')
-    first_date = transactions.first.date
-    number_of_days = compute_number_of_days
     dates = []
     i = 0
     number_of_days.times do
       dates << (first_date + i)
       i += 1
     end
-    return dates
+    return [number_of_days, dates]
   end
 
-  # TODO : Permet de créer le hash complet avec les actifs du pilot pour chaque jour depuis le début
+  # Permet de créer le hash complet avec les actifs du pilot pour chaque jour depuis le début
   def define_assets
     return [0] if self.transactions == []
 
-    days = get_dates
-    get_history(compute_number_of_days)
+    @transactions = self.transactions.order('date ASC')
+    dates = define_dates(@transactions)[1]
+    get_history(define_dates(@transactions)[0])
 
-    daily_hash = days.map do |day|
-      create_hash(day)
+    daily_hash = dates.map do |date|
+      create_hash(date)
     end
-
     p daily_hash
     return daily_hash
   end
 
-  # # TODO: Permet de calculer la valeur total du pilot à une date donnée
+  # Permet de calculer la valeur total du pilot pour un hash (jour) donné
   def total_value(hash)
     total_value = 0
     keys = hash.keys
@@ -149,28 +160,26 @@ class Dashboard < ApplicationRecord
     return total_value
   end
 
-  # # TODO: Permet de calculer la pnl totale du pilot à une date donnée
-  # def total_pnl(assets)
-  #   # total_pnl = 0
-  #   # assets.keys.each do |key|
-  #   #   pnl = assets[key][:pnl]
-  #   #   total_pnl += pnl
-  #   # end
-  #   return 0
-  # end
+  def charts
+    transactions = self.transactions.order('date ASC')
+    assets = define_assets
+    chart_values = values(transactions, assets)
+    pie_values = pie(assets)
+    return [chart_values, pie_values]
+  end
 
-  # # TODO: Permet de générer les données pour le graph
-  def values
-    dates = get_dates
-    array = define_assets
+  # Permet de générer les données pour le graph
+  def values(transactions, assets)
+    dates = define_dates(transactions)[1]
     values = dates.map do |date|
-      [date, total_value(array[dates.index(date)])]
+      [date, total_value(assets[dates.index(date)])]
     end
     return values
   end
 
-  def percentage
-    assets = define_assets.last
+  # Permet de calculer le pourcentage que représente chaque asset dans le pilot pour aujourd'hui
+  def percentage(assets)
+    assets = positive_assets(assets.last)
     total_value = total_value(assets)
     keys = assets.keys
     keys.each do |key|
@@ -179,9 +188,9 @@ class Dashboard < ApplicationRecord
     return assets
   end
 
-  def pie
-    assets = percentage()
-    p assets
+  # Permet de générer les valeurs pour le pie chart du pilot (date = aujourd'hui)
+  def pie(assets)
+    assets = percentage(assets)
     keys = assets.keys
     pie_array = keys.map do |key|
       {
@@ -193,6 +202,7 @@ class Dashboard < ApplicationRecord
     return pie_array
   end
 
+  # Permet de calculer la PnL total d'un pilot pour un hash (jour) donné
   def total_pnl(asset_hash)
     keys = asset_hash.keys
     total = 0
@@ -200,5 +210,35 @@ class Dashboard < ApplicationRecord
       total += asset_hash[key][:pnl]
     end
     return total
+  end
+
+  def sparklines
+    keys = define_assets.last.keys
+    prices = {}
+    keys.each do |key|
+      history = PriceHistory.where("id_name = '#{key}'")
+      prices[key] = history.map do |value|
+        [value.date, value.price.round(2)]
+      end
+    end
+    prices = order_prices(prices)
+    return prices
+  end
+
+  def order_prices(prices)
+    keys = prices.keys
+    keys.each do |key|
+      prices[key] = prices[key].sort { |a, b| a[0] <=> b[0] }
+    end
+    return prices
+  end
+
+  def positive_assets(assets)
+    keys = assets.keys
+    positive_assets = {}
+    keys.each do |key|
+      positive_assets[key] = assets[key] if assets[key][:quantity].positive?
+    end
+    return positive_assets
   end
 end
